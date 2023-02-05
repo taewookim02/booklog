2080a093a75db267186d36370fce3749ae653bc1
Tae Woo Kim <taewookim02@gmail.com>
2023-02-06 00:23:43 +0800

    Add basic layout

---
+++ script.js
@@ -0,0 +1,27 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? 'read already' : 'not read yet'
+    }`;
+  };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+
+console.log(theHobbit.title);
+
+
+function addBookToLibrary(book) {
+  // Add Book object to myLibrary array
+  myLibrary.push(book);
+}
+
+addBookToLibrary(theHobbit);
+console.log(myLibrary);
+
