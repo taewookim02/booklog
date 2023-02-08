d4f6db53aa4b17d891290d21d52346658eca494c
Tae Woo Kim <taewookim02@gmail.com>
2023-02-08 22:05:30 +0800

    Add book load function

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function Book(title, author, pages, read) {
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 function addBookToLibrary(book) {
-  // Add Book object to myLibrary array
   myLibrary.push(book);
 }
 
@@ -27,7 +26,10 @@ const modalContent = document.querySelector(".modal__content");
 const buttonClose = document.querySelector(".button__close");
 const cardFirst = document.querySelector(".card__first");
 const form = document.querySelector(".form");
-const formTitle = document.querySelector(".form__title");
+let formTitle = document.querySelector(".form__title");
+let formAuthor = document.querySelector(".form__author");
+let formPages = document.querySelector(".form__pages");
+let formRead = document.querySelector(".form__read").checked;
 
 modalContent.addEventListener("click", (e) => {
   e.stopPropagation();
@@ -43,12 +45,129 @@ buttonClose.addEventListener("click", (e) => {
 
 cardFirst.addEventListener("click", (e) => {
   overlay.classList.toggle("closed");
+  formTitle.focus();
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  myLibrary.forEach((book) => {
+    initialBookLoad(book);
+  });
 });
 
 // forms
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(formTitle.value);
+  updateFormValues();
+
+  myLibrary.push(
+    new Book(formTitle.value, formAuthor.value, formPages.value, formRead)
+  );
+
+  addBookToView(myLibrary);
+  clearFormValues();
+  overlay.classList.toggle("closed");
 });
 
-// Add book
+function clearFormValues() {
+  formTitle.value = "";
+  formAuthor.value = "";
+  formPages.value = "";
+  document.querySelector(".form__read").checked = false;
+}
+
+function updateFormValues() {
+  formTitle = document.querySelector(".form__title");
+  formAuthor = document.querySelector(".form__author");
+  formPages = document.querySelector(".form__pages");
+  formRead = document.querySelector(".form__read").checked;
+}
+
+function addBookToView(bookList) {
+  const book = bookList[bookList.length - 1];
+
+  // Add book by using Card HTML
+  const newElementHTML = `
+  <div class="card">
+          <div class="card__heading">
+            <h2>${book.title}</h2>
+            <div class="card__heading--icons">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#333333" viewBox="0 0 256 256" class="card__edit">
+                <rect width="256" height="256" fill="none"></rect>
+                <path
+                  d="M96,216H48a8,8,0,0,1-8-8V163.3a7.9,7.9,0,0,1,2.3-5.6l120-120a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,1,0,11.4Z"
+                  fill="none" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+                <line x1="136" y1="64" x2="192" y2="120" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <polyline points="216 216 96 216 40.5 160.5" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></polyline>
+                <line x1="164" y1="92" x2="68" y2="188" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#333333" viewBox="0 0 256 256" class="card__delete">
+                <rect width="256" height="256" fill="none"></rect>
+                <line x1="216" y1="56" x2="40" y2="56" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <line x1="104" y1="104" x2="104" y2="168" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <line x1="152" y1="104" x2="152" y2="168" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <path d="M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56" fill="none" stroke="#333333"
+                  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+                <path d="M168,56V40a16,16,0,0,0-16-16H104A16,16,0,0,0,88,40V56" fill="none" stroke="#333333"
+                  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+              </svg>
+            </div>
+          </div>
+          <p>Author: ${book.author}</p>
+          <p>${book.pages} ${book.pages > 1 ? "pages" : "page"}</p>
+          <p>${book.read ? "✅Finished reading" : "🟡Currently reading"}</p>
+        </div>
+  `;
+  // insert card HTML
+  cardFirst.insertAdjacentHTML("afterend", newElementHTML);
+}
+
+function initialBookLoad(book) {
+  const newElementHTML = `
+  <div class="card">
+          <div class="card__heading">
+            <h2>${book.title}</h2>
+            <div class="card__heading--icons">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#333333" viewBox="0 0 256 256" class="card__edit">
+                <rect width="256" height="256" fill="none"></rect>
+                <path
+                  d="M96,216H48a8,8,0,0,1-8-8V163.3a7.9,7.9,0,0,1,2.3-5.6l120-120a8,8,0,0,1,11.4,0l44.6,44.6a8,8,0,0,1,0,11.4Z"
+                  fill="none" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+                <line x1="136" y1="64" x2="192" y2="120" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <polyline points="216 216 96 216 40.5 160.5" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></polyline>
+                <line x1="164" y1="92" x2="68" y2="188" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#333333" viewBox="0 0 256 256" class="card__delete">
+                <rect width="256" height="256" fill="none"></rect>
+                <line x1="216" y1="56" x2="40" y2="56" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <line x1="104" y1="104" x2="104" y2="168" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <line x1="152" y1="104" x2="152" y2="168" fill="none" stroke="#333333" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="16"></line>
+                <path d="M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56" fill="none" stroke="#333333"
+                  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+                <path d="M168,56V40a16,16,0,0,0-16-16H104A16,16,0,0,0,88,40V56" fill="none" stroke="#333333"
+                  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
+              </svg>
+            </div>
+          </div>
+          <p>Author: ${book.author}</p>
+          <p>${book.pages} ${book.pages > 1 ? "pages" : "page"}</p>
+          <p>${book.read ? "✅Finished reading" : "🟡Currently reading"}</p>
+        </div>
+  `;
+  // insert card HTML
+  cardFirst.insertAdjacentHTML("afterend", newElementHTML);
+}
+
+// Edit book data
+// let myLibrary = [];
