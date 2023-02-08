b059b74c06712e2adcc5fb3109eadb31196adca5
Tae Woo Kim <taewookim02@gmail.com>
2023-02-08 10:53:43 +0800

    Add modal closing function

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? 'read already' : 'not read yet'
+      this.read ? "read already" : "not read yet"
     }`;
   };
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-
-console.log(theHobbit.title);
-
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 function addBookToLibrary(book) {
   // Add Book object to myLibrary array
@@ -23,8 +20,35 @@ function addBookToLibrary(book) {
 }
 
 addBookToLibrary(theHobbit);
-console.log(myLibrary);
 
 ////////////////////////
+const overlay = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
+const buttonClose = document.querySelector(".button__close");
+const cardFirst = document.querySelector(".card__first");
+const form = document.querySelector(".form");
+const formTitle = document.querySelector(".form__title");
 
+modalContent.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
 
+overlay.addEventListener("click", (e) => {
+  overlay.classList.toggle("closed");
+});
+
+buttonClose.addEventListener("click", (e) => {
+  overlay.classList.toggle("closed");
+});
+
+cardFirst.addEventListener("click", (e) => {
+  overlay.classList.toggle("closed");
+});
+
+// forms
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(formTitle.value);
+});
+
+// Add book
